0f188393cc641cefd7bf5a007f37c4a61bcaf469
mannysanz715 <mannysanz715@gmail.com>
2022-12-21 12:34:25 -0700

    Create board

---
+++ js/app.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+
+// ? -------------------- consts ---------------------------
+const winningCombos = [[]]
+
+
+
+
+
+// ? -----------------  CachedElements -------------
+const gameBoard = document.querySelector('.board-container')
+
+
+
+
+// ? ----------------------- EventListeners ---------------
+
+gameBoard.addEventListener('click', event =>{
+  console.log(`{event.target.id} `)
+})
+
+console.log(gameBoard)
+
+
+
+// ? ----------------------------------- Functions ---------------------------
+
+init()
+
+function init(){
+  
+  render()
+}
+
+function render(){
+
+}
+
+function updateBoard(){
+
+}
+
+function checkForWinner(){
+
+}
+
+function checkForTie(){
+
+}
+
+function dropPiece(){
+
+}
+
+function switchPlayer(){
+
+}
+
+function collisionDetection(){
+
+}
+
+function updateMessage (){
+
+}
+
