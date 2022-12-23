624e2e29d7e89b4a03caf7ee3830084a3b1d2340
mannysanz715 <mannysanz715@gmail.com>
2022-12-22 16:33:13 -0800

    Fix dropPiece Ligic

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@
 // ? -------------------- consts ---------------------------
 const winningCombos = [[]]
 
-const player1 = '#a63a50'
-const player2 = '#E6AA68'
-const boardArr = []
+let player1 = '#a63a50'
+let player2 = '#E6AA68'
+let boardArr = []
 let currentPlayer
 let winner
 
@@ -15,11 +15,15 @@ const gameBoard = document.querySelector('.board-container')
 
 const boardSlots = document.querySelectorAll('.circle')
 
+const resetBtn = document.getElementById('reset')
+
+const message = document.getElementById('message')
 
 // ? ----------------------- EventListeners ---------------
 
-gameBoard.addEventListener('click', changeColor)
+gameBoard.addEventListener('click', play)
 
+resetBtn.addEventListener('click', init)
 
 
 
@@ -32,8 +36,11 @@ init()
 function init () {
   currentPlayer = 1
   winner = false
+  boardSlots.forEach(element =>{
+    element.style.backgroundColor = ''
+  })
+  boardArr = []
   createBoard()
-  render()
 }
 
 function createBoard(){
@@ -43,17 +50,26 @@ function createBoard(){
 }
 
 
-function render(){
-
+function play(event){
+  let clickedSlot = event.target
+  if(clickedSlot.parentElement.className !== 'board-container'){
+    dropPiece(event)
+    updateMessage()
+  }
 }
 
-function changeColor(event){
-
-
-  if(column !== 'board-container'){
-    console.log(row, column) 
-    
-  }
+function dropPiece(event){
+  let clickedSlot = event.target
+  let column = event.target.parentElement.classList[0]
+  for(let i = 5; i > -1; i--){
+    if(boardArr[column][i] == null){
+      boardArr[column][i] = 1
+      if(currentPlayer === 1) clickedSlot.parentElement.children[i].style.backgroundColor = player1 
+      else if(currentPlayer === -1) clickedSlot.parentElement.children[i].style.backgroundColor = player2
+      switchPlayer()
+      return
+      }
+    }
 }
 
 function checkForWinner(){
@@ -61,18 +77,13 @@ function checkForWinner(){
 }
 
 function checkForTie(){
-
-}
-
-function dropPiece(){
-
+  if(boardArr.every(el => el != null)){
+    tie = true
+  }
 }
 
 function switchPlayer(){
-  if(!winner){
     currentPlayer *= -1
-  }
-
 }
 
 function collisionDetection(){
@@ -80,6 +91,11 @@ function collisionDetection(){
 }
 
 function updateMessage (){
+  if(currentPlayer === 1){
+    message.textContent = "Red's turn"
+  }else if(currentPlayer === -1){
 
+    message.textContent = "Yellow's turn"
+  }
 }
 
